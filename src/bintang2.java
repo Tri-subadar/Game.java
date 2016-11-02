d3fb0bae4a13b9954ce6fc09571a3ff08664a8db
Tri-subadar <3tri.chicken@gmail.com>
2016-11-02 21:54:26 +0700

    12141388 input batas karakter

--- src/bintang2.java
+++ src/bintang2.java
@@ -1,4 +1,12 @@
 
 import java.util.Scanner;
 
-
+public class bintang2 {
+    public static void main(String [] args){
+        int panjang ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Tentukan panjang karakter : ");
+        panjang = input.nextInt();
+        
+    }
+}
