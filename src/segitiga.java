b4cbf3c4415421c26ff3a541883956bcc61a14c8
Tri-subadar <3tri.chicken@gmail.com>
2016-11-03 20:02:43 +0700

    12141388 awal program

---
+++ src/segitiga.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+//import static java.time.Clock.system;
+import java.util.Scanner;
+public class segitiga {
+    public static void main (String[] args){
+        
+        
+    }
+}
