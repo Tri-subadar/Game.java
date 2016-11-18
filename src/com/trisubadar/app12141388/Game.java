615f700534c33554b5e78e3a400bbdd05c99f62e
Tri-subadar <3tri.chicken@gmail.com>
2016-11-18 20:50:34 +0700

    12141388 masukan nama

---
+++ src/com/trisubadar/app12141388/Game.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.trisubadar.app12141388;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Game {
+    
+ public String nama;
+ public int gameAngka;
+ public byte level;
+ public char jawab;
+ public int randomisedNumber;
+ public int jumlahTebakan;
+ 
+public void Tebakan(){
+    Scanner game = new Scanner(System.in);
+    System.out.println  ("Masukan Nama Anda untuk memulai atau tekanan 'Y' untuk keluar. Kemudian 'enter'");
+    this.nama = game.nextLine();
+    
+    
+    
+    
+    
+}
+ 
+
+
+ 
+    
+}
