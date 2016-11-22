6ec18108a51a004c21c18637e9358ca547eba4a8
Tri-subadar <3tri.chicken@gmail.com>
2016-11-22 21:15:10 +0700

    12141388 untuk memasukan fungsi

--- src/com/trisubadar/app12141388/game.java
+++ src/com/trisubadar/app12141388/game.java
@@ -11,3 +11,20 @@ package com.trisubadar.app12141388;
  * @author user
  */
  
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+/**
+ *  public static void main(String[] args) {
+       
+         
+        Scanner input = new Scanner(System.in);
+        Scanner input2= new Scanner(System.in); 
+        Random angkaRandom=new Random(); 
+        String cariY; 
+ * @author user
+ */
+
+public class game {
+   
+}
